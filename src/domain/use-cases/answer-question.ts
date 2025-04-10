70e54cc3574c1d64a34d97dfea5198ea82bff889
Matheus Valandro <msvalandro@proton.me>
2025-04-10 20:18:48 +0100

    feat: enhance entity models with required properties and improve exports

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -12,7 +12,7 @@ export class AnswerQuestionUseCase {
     questionId,
     content,
   }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content)
+    const answer = new Answer({ content, authorId: instructionId, questionId })
 
     return answer
   }
