804352809c8b0ba7412dc7de00715c3b667d63c6
Matheus Valandro <msvalandro@proton.me>
2025-04-10 20:56:35 +0100

    refactor: implement Entity base class for domain entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '../../core/entities/entity'
 
 type AnswerProps = {
   content: string
@@ -6,16 +6,8 @@ type AnswerProps = {
   questionId: string
 }
 
-export class Answer {
-  public id: string
-  public content: string
-  public authorId: string
-  public questionId: string
-
-  constructor({ content, authorId, questionId }: AnswerProps, id?: string) {
-    this.content = content
-    this.authorId = authorId
-    this.questionId = questionId
-    this.id = id ?? randomUUID()
+export class Answer extends Entity<AnswerProps> {
+  get content() {
+    return this.props.content
   }
 }
