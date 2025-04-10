a3be97daa1defb1946bf75143ccaf01cceb0cff1
Matheus Valandro <msvalandro@proton.me>
2025-04-10 20:42:40 +0100

    feat: add Slug value object and update Question entity

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest'
+import { Slug } from './slug'
+
+describe('ValueObjects: Slug', () => {
+  it('should be able to create a new slug from text', () => {
+    const slug = Slug.createFromText('Example question title')
+
+    expect(slug.value).toBe('example-question-title')
+  })
+})
