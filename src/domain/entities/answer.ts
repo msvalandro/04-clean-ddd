783199cd66f2ce27ce05df59de46387f63fe37c0
Matheus Valandro <msvalandro@proton.me>
2025-08-15 18:33:24 +0100

    refactor: add getters and setters to entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -11,10 +11,6 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
-  get content() {
-    return this.props.content
-  }
-
   static create(
     props: Optional<AnswerProps, 'createdAt'>,
     id?: UniqueEntityID,
@@ -28,5 +24,38 @@ export class Answer extends Entity<AnswerProps> {
     )
 
     return answer
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
   }
 }
