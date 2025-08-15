43964df6f9ba7bb6795bfd141970575bd54b2385
Matheus Valandro <msvalandro@proton.me>
2025-08-15 18:06:49 +0100

    refactor: use unique entity id on answer/question entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface AnswerProps {
   content: string
-  authorId: string
-  questionId: string
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import type { Slug } from './value-objects/slug'
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface QuestionProps {
   title: string
   content: string
   slug: Slug
-  authorId: string
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
