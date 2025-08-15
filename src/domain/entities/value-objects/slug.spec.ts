c9b4d082eb9ecb38f52bfe841043bcdb82795f33
Matheus Valandro <msvalandro@proton.me>
2025-08-15 17:48:50 +0100

    feat: create slug value object

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { it, expect } from 'vitest'
+import { Slug } from './slug'
+
+it('should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Example question title')
+
+  expect(slug.value).toEqual('example-question-title')
+})
