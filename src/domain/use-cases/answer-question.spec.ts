c7eadaadde3653f291e2eb64d311d319244c5221
Matheus Valandro <msvalandro@proton.me>
2025-08-15 16:34:10 +0100

    feat: add answers repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,14 +1,20 @@
-import { test, expect } from 'vitest';
-import { AnswerQuestionUseCase } from './answer-question';
+import { test, expect } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import type { AnswersRepository } from '../repositories/answers-repository'
+import type { Answer } from '../entities/answer'
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {},
+}
 
-  const answer = answerQuestion.execute({
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'Nova resposta',
-  });
+  })
 
-  expect(answer.content).toEqual('Nova resposta');
-});
+  expect(answer.content).toEqual('Nova resposta')
+})
